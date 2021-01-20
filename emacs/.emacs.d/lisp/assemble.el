5b0b3a85052d75d397e93520e238b4dcf197ba15
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-01-20 14:03:41 -0500

    Incremental and wildcard builds with assemble

--- emacs/.emacs.d/lisp/assemble.el
+++ emacs/.emacs.d/lisp/assemble.el
@@ -2,14 +2,85 @@
 
 (require 'ox)
 (require 'ox-latex)
+(require 'seq)
 (require 'subr-x)
 
 (defvar assemble-file "assemb.el")
 
-(defmacro assemble-target (depending on deps producing out-name &rest body)
-  `(cons (mapcar 'assemble ,deps) ,@body))
+(defmacro assemble-target (name depending on deps &rest body)
+  "Defines a build target with specified dependencies.
+
+NAME is the name of the build target, which can be executed with
+(assemble \"NAME\").  Additionally, NAME should correspond to the name
+of the file being produced by this target.
+
+DEPS is a list of dependencies for this target.
+
+BODY is the body of the build target, which is responsible for
+building NAME.
+
+For example, suppose assemb.el contains
+
+(assemble-target \"foo.html\" depending on '(\"foo.org\")
+                 (defile \"foo\"
+                    (append-file-to-buffer \"foo.org\")
+                    (assemble-html \"test.html\")))
+
+Then, executing (assemble \"foo.html\") will export foo.org to html if
+foo.org has been modified since the last time it was built."
+  `(defun ,(intern name) ()
+     nil
+     nil
+     (progn
+      (message (concat "[ . ] " ,name))
+      (if (seq-reduce
+	   (lambda (acc x) (or acc x))
+	   (mapcar (lambda (dep) (assemble-ifchanged ,name dep))
+		   ,deps)
+	   nil)
+	  (progn (message (concat "[>>>] " ,name)) ,@body t)
+	nil))))
+
+(defmacro assemble-wildcard (extension depending on deps &rest body)
+  `(defun ,(intern (concat "_" extension)) (filename)
+     nil
+     nil
+     (progn
+       (message (concat "[ * ] " filename))
+       (if (seq-reduce
+	    (lambda (acc x) (or acc x))
+	    (mapcar (lambda (dep) (assemble-ifchanged filename dep))
+		    (resolve-wildcard-deps ,deps filename))
+	    nil)
+	   (let ((_filename (file-name-sans-extension filename)))
+	     (progn (message (concat "[>>>] " filename)) ,@body t))
+	 nil))))
+
+(defun resolve-wildcard-deps (deps file)
+  (mapcar (lambda (dep)
+	    (let* ((dep-name (symbol-name dep)))
+	      (if (string-prefix-p "_" dep-name)
+		  (intern (concat (file-name-sans-extension file) "."
+				  (string-remove-prefix "_" dep-name)))
+		dep)))
+	  deps))
+
+(defun assemble-ifchanged (target dep)
+  "Assemble a build dependency and report if its parent needs to be assembled"
+  (if (string-equal target (symbol-name dep))
+      (and (message (concat "[XXX] Fatal Error :: Cyclic dependency detected: "
+			    target " depends on itself!"))
+	   nil)
+    (let* ((dep-path (symbol-name dep))
+	    (extension (file-name-extension dep-path))
+	    (wildcard (intern (concat "_" extension))))
+	(if (or (fboundp dep) (fboundp wildcard))
+	    (and (if (fboundp dep) (funcall dep) (funcall wildcard dep-path))
+		(and (file-newer-than-file-p dep-path target) t))
+	(and (file-exists-p dep-path) (file-newer-than-file-p dep-path target))))))
 
 (defun assemble (target)
+  "Assemble a given target"
   (interactive "sTarget (nil): ")
   (let* ((dir (find-assemble-dir default-directory))
 	 (load-path (cons dir load-path)))
@@ -33,13 +104,11 @@
 	  (fn (intern target)))
       (funcall fn))))
 
-(defun defile (target-name thunk)
-  "Perform actions in a temporary file"
-  (with-temp-buffer
-    (rename-buffer (concat target-name "---assembling"))
-    (message (concat "Assembling " target-name "..."))
-    (funcall thunk)
-    (message "DONE")))
+(defmacro defile (target-name &rest body)
+  `(with-temp-buffer
+     (rename-buffer (concat ,target-name "---assembling"))
+     (message (concat "Assembling " ,target-name "..."))
+     ,@body))
 
 (defun append-file-to-buffer (file)
   (insert-file-contents file)
@@ -56,16 +125,19 @@
 
 (defun assemble-latex (file-name &optional out-dir)
   "Produce latex pdf from buffer"
-  (simple-org-export
-   'latex
-   'org-latex-compile
-   (concat out-dir "/" file-name)))
+  (let ((out-path (if out-dir
+		      (concat out-dir "/" file-name)
+		    file-name)))
+    (simple-org-export
+     'latex
+     'org-latex-compile
+     out-path)))
 
 (defun assemble-latex-bibtex (file-base-name &optional out-dir)
   "Produce latex pdf and compile bibtex from buffer"
   (require 'org-ref)
   (let* ((pdflatex-cmd (format "pdflatex %s %s.tex"
-			       (and out-dir (concat "-output-directory=" out-dir))
+			       (if out-dir (concat "-output-directory=" out-dir) "")
 			       file-base-name))
 	 (bibtex-cmd (format "bibtex %s"
 			     (if out-dir (concat out-dir "/" file-base-name) file-base-name)))
