ab712f7f8f1ca4da6f4a7a76fbaf5c13fac5f76b
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-11-14 15:49:48 -0500

    Adds snippets for vimtex

--- neovim/nvim/lua/my-snippets.lua
+++ neovim/nvim/lua/my-snippets.lua
@@ -2,9 +2,33 @@ local function texenv(name)
   return '\\begin{' .. name .. '}\n$0\n\\end{' .. name .. '}'
 end
 
+local function texenvheader(name, label, labelprefix)
+  local label = label or false
+  local labelprefix = labelprefix or ''
+  local header = '\\begin{' .. name ..  '}'
+  if label then
+    local headerwithlabel = header .. '\\label{'
+    if labelprefix:len() == 0 then
+      return headerwithlabel
+    else
+      return headerwithlabel .. labelprefix .. ':'
+    end
+  else
+    return header
+  end
+end
+
 require'snippets'.snippets = {
   _global = {
     mc = [[\\mathcal{]]
+  },
+  tex = {
+    Al = texenvheader('align'),
+    al = texenvheader('align*'),
+    ald = texenvheader('aligned'),
+    Eq = texenvheader('equation', true, 'eq'),
+    fig = texenvheader('figure', label, 'fig'),
+    prob = '\\begin{problem}{$0}\n\\end{problem}'
   },
   markdown = {
     Al = texenv('align'),
