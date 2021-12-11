73e591065930eb57e309628f6087123399cfcd42
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2021-12-11 11:55:50 -0500

    Remaps something that overwrote digraph insert

--- neovim/nvim/lua/maps.lua
+++ neovim/nvim/lua/maps.lua
@@ -2,7 +2,7 @@ local map = vim.api.nvim_set_keymap
 
 map('n', '<leader>q', ':source %<CR>', {})
 
-map('i', '<C-k>', '<cmd>lua return require\'snippets\'.expand_or_advance(1)<CR>', {})
+map('i', '<C-j>', '<cmd>lua return require\'snippets\'.expand_or_advance(1)<CR>', {})
 
 map('n', '<Leader>f', [[<Cmd>lua require('telescope.builtin').find_files({hidden=true, layout_config={prompt_position="top"}})<CR>]], {silent=true})
 
