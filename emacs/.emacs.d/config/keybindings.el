edf3ad540408739cfb81dd57a2688b4d2bcab6a4
Harley Wiltzer <harley.wiltzer@mail.mcgill.ca>
2023-12-23 12:37:51 -0500

    Adds colorscheme switching

--- emacs/.emacs.d/config/keybindings.el
+++ emacs/.emacs.d/config/keybindings.el
@@ -1,2 +1,3 @@
 (global-set-key (kbd "M-e") 'eshell)
 (global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "M-<f6>") 'harwiltz/toggle-theme)
--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-(set-face-attribute 'default nil :height 120 :family "Inconsolata") ; Remove later
+(set-face-attribute 'default nil :height 120 :family "Ubuntu Mono") ; Remove later
 
 (add-to-list 'load-path "~/.emacs.d/config")
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -13,10 +13,39 @@
 (load "org-roam-config.el")
 (load "keybindings.el")
 (and (file-exists-p harwiltz/agenda-dir) (load "agenda.el"))
+
 (load custom-file t)
-;; (load-theme 'modus-operandi)
-(setq catppuccin-flavor 'mocha)
-(load-theme 'catppuccin)
+
+(defadvice load-theme (before theme-dont-propagate activate)
+  (mapc #'disable-theme custom-enabled-themes))
+
+;; Nice themes:
+;; - ample (dark)
+;; - modus-operandi-tinted (light)
+;; - modus-vivendi (dark)
+;; - gruvbox (dark)
+;; - doom-gruvbox (dark)
+;; - kaolin-breeze (light)
+;; - kaolin-shiva (dark)
+;; - spacemacs-dark (dark)
+;; - spacemacs-light (light)
+
+(setq harwiltz/dark-theme 'spacemacs-dark)
+(setq harwiltz/light-theme 'spacemacs-light)
+(setq harwiltz/apply-dark-theme t)
+
+(defun harwiltz/load-theme ()
+  (interactive)
+  (if harwiltz/apply-dark-theme (load-theme harwiltz/dark-theme) (load-theme harwiltz/light-theme)))
+
+(defun harwiltz/toggle-theme ()
+  (interactive)
+  (setq harwiltz/apply-dark-theme (not harwiltz/apply-dark-theme))
+  (harwiltz/load-theme))
+
+(harwiltz/load-theme)
+;; (setq catppuccin-flavor 'mocha)
+;; (load-theme 'catppuccin)
 
 (blink-cursor-mode 0)
 (scroll-bar-mode -1)
@@ -29,4 +58,4 @@
 (custom-theme-set-faces
  'user
  '(variable-pitch ((t (:family "Linux Libertine" :height 120 :weight thin))))
- '(fixed-pitch ((t (:family "Inconsolata" :height 120)))))
+ '(fixed-pitch ((t (:family "Ubuntu Mono" :height 120)))))
